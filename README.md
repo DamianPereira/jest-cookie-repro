2a705537e526b5dd76b1791f7e20a126e0d27a68
Damian Pereira <damian.pereira@medialab.la>
2025-09-17 23:26:21 -0300

    Migrate to vitest

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a repo to reproduce issue in jestjs/jest#15727
+
+Last commit migrates from jest to vite, which fixes the issue. To get the real repro checkout 1 commit before that.
--- cookies.test.js
+++ cookies.test.js
@@ -1,7 +1,7 @@
 describe("cookie expiring", () => {
     it("should not be expired when expire date is in the future", () => {
-        jest.useFakeTimers();
-        jest.setSystemTime(new Date("Mon, 01 Jan 2023 12:00:00 GMT"));
+        vi.useFakeTimers();
+        vi.setSystemTime(new Date("Mon, 01 Jan 2023 12:00:00 GMT"));
         document.cookie = "foo=bar; expires=Mon, 02 Jan 2023 12:00:00 GMT";
         expect(document.cookie).toBe("foo=bar");
     });
